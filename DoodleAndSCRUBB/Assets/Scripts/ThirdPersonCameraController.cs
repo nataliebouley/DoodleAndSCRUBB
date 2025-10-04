b8c2244d103d0de26ebc4c75a1af725e9ad87530
nataliebouley <83614901+nataliebouley@users.noreply.github.com>
2025-10-03 21:12:06 -0700

    Remove Old Assets

--- DoodleAndSCRUBB/Assets/Scripts/ThirdPersonCameraController.cs
+++ DoodleAndSCRUBB/Assets/Scripts/ThirdPersonCameraController.cs
@@ -4,31 +4,5 @@ using UnityEngine;
 
 public class ThirdPersonCameraController : MonoBehaviour
 {
-    [SerializeField] private float zoomSpeed = 2.0f;
-    [SerializeField] private float zoomLerpSpeed = 10.0f;
-    [SerializeField] private float minDistance = 3.0f;
-    [SerializeField] private float maxDistance = 15.0f;
 
-    private PlayerControls controls;
-
-    private CinemachineCamera cam;
-    private CinemachineOrbitalFollow orbital;
-    private Vector2 scrollDelta;
-
-    private float targetZoom;
-    private float currentZoom;
-
-    private
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        controls = new PlayerControls();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
